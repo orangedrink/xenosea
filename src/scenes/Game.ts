9b41794abe6e6f0db4d8a525b256cc8bc1c443ea
orangedrink <orangedrink@users.noreply.github.com>
2022-06-24 20:42:36 -0500

    Adds creatAnim function

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -4,13 +4,22 @@ export default class Demo extends Phaser.Scene {
   constructor() {
     super('GameScene');
   }
+  private animations: any;
+  private createAnim(key:string, assetKey:string, frames:any) {
+    const anim = this.anims.create({
+      key: key,
+      frames: this.anims.generateFrameNumbers(assetKey, { frames: frames }),
+      frameRate: 16
+    })
+    this.animations.push(anim)
+  }
 
   preload() {
   }
 
   create() {
     const logo = this.add.image(400, 300, 'logo');
-
+    this.createAnim('player-l1-idle','player-l1',[0]);
 
   }
 }
